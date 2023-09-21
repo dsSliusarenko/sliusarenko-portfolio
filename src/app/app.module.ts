3cc29f906faa3a639802b24b752bd507c265cf0f
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-09-20 19:14:43 -0600

    created basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NavigationModule} from "./navigation/navigation.module";
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavigationModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
