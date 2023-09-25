a156d700ff66307af2aaf151d98489fb103fbe0a
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-09-24 19:46:37 -0600

    added links to side-bar component icons. Changed backgraound variable name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
   imageProfileAddress: 'assets/images/profile-avatar.png',
-  myName: 'Dmytro Sliusarenko'
+  myName: 'Dmytro Sliusarenko',
+  gitHubUrl: 'https://github.com/dsSliusarenko',
+  linkedInUrl: 'https://www.linkedin.com/in/dsliusarenko/',
+  instagramUrl: 'https://www.instagram.com/sliusarenko_dima',
+  discordUrl: 'https://discordapp.com/users/853352492175589417'
 };
